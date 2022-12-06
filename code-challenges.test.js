79483d5e25ad9a8fd6fe25fdf4618e4fa13e8471
sayoung0820 <shannonashley78@gmail.com>
2022-12-06 04:17:18 -0500

    week 5 assessment completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
 
+//const { it } = require("node:test")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -21,7 +23,83 @@ const secretCodeWord2 = "Gobbledygook"
 const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
+
+describe("decode", () => {
+    it("takes in a string and returns a string with a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.", () => {
+        let secretCodeWord1 = "Lackadaisical"
+        let secretCodeWord2 = "Gobbledygook"
+        let secretCodeWord3 = "Eccentric"
+    expect(decode(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+    expect(decode(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+    expect(decode(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+})
+
+// ReferenceError: decode is not defined
+
 // b) Create the function that makes the test pass.
+
+// Pseudocode:
+// one parameter, a string
+// returning new string with same length
+// create empty string for new string?
+// swap letter for number, using .replace
+// or use .includes if possible on strings
+// return the new string
+
+
+    // FIRST ATTEMPT
+
+const decode = (string) => {
+    if (string.includes("a") &&
+    string.includes("e") &&
+    string.includes("i") &&
+    string.includes("o")) {
+ let newString = string.replace("a", 4)
+        .replace("e", 3)
+        .replace("i", 1)
+        .replace("o", 0)
+        return newString
+    }
+}
+
+    // SECOND ATTEMPT
+
+const decode = (string) => {
+    let newString = " "
+    newString = string
+    .replaceAll('a', '4')
+    .replaceAll('e', '3')
+    .replaceAll('i', '1')
+    .replaceAll('o', '0')
+    return newString
+}
+
+    // THIRD ATTEMPT
+
+    const decode = (string) => {
+    return string.replace(/[a]/g, '4')
+                 .replace(/[e]/g, '3')
+                 .replace(/[i]/g, '1')
+                 .replace(/[o]/g, '0')
+}
+
+    // FOURTH ATTEMPT
+
+    const decode = (string1, string2, string3) => {
+    let getStrings = (string1, string2, string3)
+    newString = getStrings.replace(/[a]/g, '4')
+                      .replace(/[e]/g, '3')
+                      .replace(/[i]/g, '1')
+                      .replace(/[o]/g, '0')
+                      return newString
+
+    // NOTES ABOUT PROBLEM #1
+    // First attempt: Worked but only on the first occurrence. At first, I used .replace only, and receieved "L4ckadaisical", which meant it only changed the first occurrence of 'a'. Refactored and used .replaceAll. The letters in "Lackadaisical" were replaced for all occurences, but then it just stopped reading beyond the first word in the code, error: Expected: "3cc3ntr1c" Received: "Ecc3ntr1c"
+    // Tried again using regex, using the 'g'/global flag to try to catch all occurrences but got same issue of only reading the first line of the code then not proceeding. The letters are being replaced in first code word but no other code words are being read. I believe the issue is Javascript cannot return multiple values, so will maybe need to wrap the values in an object? 
+
+
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
@@ -34,19 +112,167 @@ const letterA = "a"
 const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
-// b) Create the function that makes the test pass.
+
+// input 1 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+// input2 = "a"
+// input3 = "e"
+
+describe("wordsArr", () => {
+    it("takes in an array of words and a single letter and returns an array of all the words containing that particular letter", () => {
+        let fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+        let letterA = "a"
+        let letterE = "e"
+      expect(wordsArr(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+      expect(wordsArr(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+  })
+
+  // ReferenceError: wordsArr is not defined
+
+  // b) Create the function that makes the test pass.
+
+  // Pseudocode
+  // create function that takes in an array of words and a string, and returns an array of the words containing the string
+  // two parameters, array and letter/string
+  // tried to use .includes but appears array.includes does not allow you to search for a fraction of an item, only whole items
+  // need to iterate through array, search each value/item for the letter
+  // tried .filter to return all of the array elements that contain the letter
+
+    // FIRST ATTEMPT
+    
+const wordsArr = (array, string) => {
+    let newArray = array.filter(element => element.includes(string))
+    return newArray
+}
+
+
+    // SECOND ATTEMPT
+
+const wordsArr = (array, letter) => {
+    const newArray = []
+    for (let i = 0; i < array.length; i++)
+    if (array[i].includes(letter)) {
+    return newArray.push(array[i])
+  }
+}
+
+    // THIRD ATTEMPT
+
+const wordsArr = (array, letter) => {
+       var sublist = []
+       var word
+    for (let i = 0; i < array.length; i++) {
+       word = array[i]
+    if (word.indexOf(letter) > -1) sublist.push(word) 
+    return sublist
+    }
+  }
+
+    // GLUTTON FOR PUNISHMENT ATTEMPT LOL
+
+    const wordsArr = (array, string1, string2) => {
+    if (array.find(element => {
+    if (element.includes(string1 || string2)) {
+    return element
+  }
+
+
+    // NOTES ON PROBLEM #2
+    
+// Tried this a few ways and could not get it to pass! 
+// last error:  - Expected  - 1
+//     + Received  + 0
+
+//     Array [
+//       "Mango",
+//   -   "Apricot",
+//       "Peach",
+//     ]
+// Moving on and will come back and try refactoring again
+
+
+
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
 const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
+// // Expected output: true
 const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
+// // Expected output: false
 const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
+// // Expected output: false
 const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
-// b) Create the function that makes the test pass.
+describe("fullHouse", () => {
+    it("takes in an array of 5 numbers and determines whether or not the array is a 'full house'", () => {
+        const hand1 = [5, 5, 5, 3, 3]
+        const hand2 = [5, 5, 3, 3, 4]
+        const hand3 = [5, 5, 5, 5, 4]
+        const hand4 = [7, 2, 7, 2, 7]
+      expect(fullHouse(hand1)).toEqual(true)
+      expect(fullHouse(hand2)).toEqual(false)
+      expect(fullHouse(hand3)).toEqual(false)
+      expect(fullHouse(hand4)).toEqual(true)
+    })
+  })
+
+  //  ReferenceError: fullHouse is not defined
+
+  //A full house is exactly one pair and one three of a kind.
+
+    // b) Create the function that makes the test pass.
+
+    // Pseudocode:
+    // create a function that takes in an array of 5 numbers and determines whether the numbers have one pair of the smae number and three of the same number
+    // one parameter, array
+    // returning a boolean value
+    // most likely use a for loop and conditional statement
+    // use /length to iterate
+    // tried .slice but test failed with "expected true received false"
+    // refactored and switched the return values, same error but opposite
+    // 
+
+        // FIRST ATTEMPT
+
+    const fullHouse = (array) => {
+        const copy = array.slice();
+        for(let i = 0; i < array.length; ){
+           const el = copy.splice(i, 1)[0];
+           if(copy.includes(el)){
+              copy.splice(copy.indexOf(el), 1);
+              if(copy.includes(el)){
+                 return false;
+              }
+           }else{
+              i++;
+           }
+        };
+        return true;
+     };
+
+    
+    // SECOND ATTEMPT
+
+    // Tried another method found online using an empty variable to hold the count of each value
+    // used for loop to iterate through the array
+    // created index for object values 
+    // created variable for values 
+    // created conditional for values to evaluate the values against the full house numbers 
+    // return true or false 
+
+     const fullHouse = (array) => {
+        let countObj = {}
+        for(let x of array){
+          countObj[x] = (countObj[x] || 0) + 1;
+        }
+        let vals = Object.values(countObj);
+        if((vals[0] === 2 && vals[1] === 3) || (vals[1] === 2 && vals[0] === 3)){
+          return true;
+        }
+        return false;
+      }
+
+
--- interview-questions.md
+++ interview-questions.md
@@ -10,31 +10,52 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Your answer:
 
+Calling super in a class will allow you to access the main class and it's properties. This gives access to the methods used by the main class which makes it easier for modification. Super is not a variable.
+
 Researched answer:
+
+Super is a keyword related to the idea of inheritance in Javascript, but is used in many programming languages. In general, it provides a reference to a parent class. Calling super in a child class in Javascript, will allow you to access the parent class and it's properties and methods. It is mainly used to access a variable, method, or construtor in the parent class. Super can be used in two ways. The first is as a function call. This passes an argument in a function. This can accept all the arguments that were used to create the parent constructor. The second is as a "property lookup" or an object. 
 
 2. What is a gem?
 
 Your answer:
 
+A gem is a file in a library or database that is used to store data. Gems are prepopulated in the ruby app and essentially make the code work. 
+
 Researched answer:
+
+Ruby gems are built in open sourced libraries that contain custom Ruby code methods written by other coders. They are packaged with more data that allows a programmer to use the code contained in the gem within their own code or program, without having to insert the code. This makes writing the code much faster and easier. Gems can be used for many different purposes and there are currenlty over 160,000 gems available.
 
 3. What is a relational database? Are there other kinds of databases?
 
 Your answer:
 
+Relational databases are databases that store information in tables. The tables consist of rows and columns, and can have multiple rows/columns.  
+
 Researched answer:
+
+Relational databases are databases that hold information in tables of rows and columns. It has the ability to establish relationships by joining other tables. This makes it easy to understand the connection between data types. Relational databases define the name and data of each column using a schema - the description of the layout of all the tables in a database including the name and data type of each column in each table. Relational databases are typically used for processing and managing transactions, but can be used for many other purposes. While the main benefit of a relational database is to connect data from different tables, it also has many other benefits such as being easy to navigate, saving time and money, and helps to eliminate duplicate information. 
 
 4. What are primary keys? Why are they important?
 
 Your answer:
 
+Primary keys are unique keys that gives each entry on the table a unique indentifier. This prevents confusing data that have similar information. 
+
 Researched answer:
+
+A primary key is a unique column in a relational database that is distinctive for each record. It is used as a unique indentifier that contains unique values and indentifies each row in a table. It can also be called a primary keyword and can be used to compare, sort, and store records. Choosing the primary key is the most important step in the database process. They can consist of a single attribute or multiple attributes together. Primary keys should never be changed and cannot contain NULL values. 
 
 5. What are the HTTP verbs? What is each verb's corresponding CRUD action?
 
 Your answer:
 
+The most commonly used HTTP verbs are POST, PUT, PATCH, and DELETE. They correspond to create, read, update, and delete. 
+
 Researched answer:
+
+There are total of 39 total HTTP verbs, and each one provides a method for interactions. The most common HTTP methods are POST which corresponds to the CRUD method Create; GET which corresponds to the CRUD method Read, PUT which corresponds to the CRUD method Update or Replace, PATCH which corresponds to the CRUD method Update/Modify, and DELETE which corresponds to DELETE.
+
 
 ## Looking Ahead: Terms for Next Week
 
@@ -42,10 +63,20 @@ Research and define the following terms to the best of your ability.
 
 1. RESTful routes:
 
+REST stands for Representational State Transfer. RESTful routing is a set of standards used in many different languages to create efficient, reusable routes. It maps HTTP methods or verbs and CRUD actions together in a conventional pattern.
+
 2. Ruby on Rails:
+
+Ruby on Rails is a full stack framework that contains all the tools needed to build web apps on both the front and back end. It renders HTML templates, updates databases, sends/receives emails, and maintains live pages using WebSockets which provides solid security protections for common attacks. 
 
 3. Database migrations:
 
+Migrations are a convenient way to alter your database schema over in a consistent manner, using Ruby DSL (Domain Specific Language) so that SQL doesn't have to be written by hand, allowing the schema and changes to be database independent.
+
 4. Params:
 
+Params, an alias for parameters, are variables that are defined in the method definition that represent the ability of a method to accept arguments. If we do not have the appropriate parameters then we will not be able to pass arguments to a method that will contain the data we need.
+
 5. ERB:
+
+ERB is a templating machine that allows you to mix HTML and Ruby to generate web pages using data from your database. ERB is a Rails default engine for rendering views. It provides an easy to use templating system that allows code to be added to any plain text document for the purpose of generating document information details and/or flow control.
